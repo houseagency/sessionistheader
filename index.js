9cfee9fb9c16e7a1e8a6ff3bee85b3db20b041b1
alfred <alfred@nurd.nu>
2016-09-17 23:42:59 +0000

    prettified somewhat

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
-const q = require('q');
 const cryptojs = require('crypto-js');
+const q = require('q');
+const _ = require('lodash');
 
 const sha = str => cryptojs.SHA3(str).toString();
 
 module.exports = (key_id, secret_key, payload, cb) => {
 	let deferred = q.defer();
 	let salt = cryptojs.lib.WordArray.random(64).toString();
-	let time = '' + (new Date()).getTime();
+	let time = _.now();
 	let hash = sha(secret_key + sha(salt + sha(secret_key + payload + time)));
 	deferred.resolve('ss1 keyid=' + key_id + ', hash=' + hash + ', salt=' + salt + ', time=' + time);
 	deferred.promise.nodeify(cb);
 	return deferred.promise;
 };
 
-module.exports.verify = (header, payload, keyfn, cb) => {
+module.exports.verify = (headerStr, payload, keyfn, cb) => {
 	let deferred = q.defer();
-	let h = header.toLowerCase().split(',').map(str => str.trim())
-		.reduce((pre, cur) => {
-			let [key, val] = cur.replace(/^([^: ]+: ){0,1}ss1 /, '').split('=');
-			pre[key] = val;
-			return pre;
-		}, {});
-	if (Object.keys(h).sort().join(',') !== 'hash,keyid,salt,time') {
+	let header = headerStr.replace(/^([^: ]+: ){0,1}ss1 /, '').split(/,\s*/)
+		.reduce((pre, cur) => Object.assign({}, pre, _.fromPairs([ cur.split('=') ])), {});
+	if (Object.keys(header).sort().join(',') !== 'hash,keyid,salt,time') {
 		deferred.reject(new Error('Wrong header format.'));
+	} else if (Math.abs(header['time'] - _.now()) > 86400000) {
+		deferred.reject(new Error('Too big time difference.'));
 	} else {
-		if (Math.abs(parseInt(h['time']) - (new Date()).getTime()) > 86400000) {
-			deferred.reject(new Error('Too big time difference.'));
-		} else {
-			setImmediate(() => keyfn(h['keyid'], (err, secret_key) => {
-				if (err) return deferred.reject(err);
-				if (h['hash'] !== sha(secret_key + sha(h['salt'] + sha(secret_key + payload + h['time']))))
-					return deferred.reject(new Error('Hash does not match.'));
-				deferred.resolve();
-			}));
-		}
+		setImmediate(() => keyfn(header['keyid'], (err, secret_key) => {
+			if (err) return deferred.reject(err);
+			if (header['hash'] !== sha(secret_key + sha(header['salt'] + sha(secret_key + payload + header['time']))))
+				return deferred.reject(new Error('Hash does not match.'));
+			deferred.resolve();
+		}));
 	}
 	deferred.promise.nodeify(cb);
 	return deferred.promise;
