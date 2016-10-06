5e0692785b894035bab75cbf657f3b05ce32fb68
alfred <alfred@nurd.nu>
2016-10-06 15:29:45 +0000

    fixed tests to new header format

--- test/index.js
+++ test/index.js
@@ -3,23 +3,26 @@ const header = require('../index');
 
 describe('Module', () => {
 
+	let today = (new Date()).toUTCString();
+	let yesturday = (new Date((new Date()).getTime() - (25 * 60 * 60 * 1000))).toUTCString();
+
 	describe('creating header', () => {
 
 		it('should provide a callback interface', done => {
-			header('my key id', 'my secret key', 'the payload', (err, data) => {
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today, (err, data) => {
 				done();
 			});
 		});
 
 		it('should provide a promise interface', done => {
-			header('my key id', 'my secret key', 'the payload')
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today)
 			.then(data => {
 				done();
 			});
 		});
 
 		it('should return a string', done => {
-			header('my key id', 'my secret key', 'the payload')
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today)
 			.then(data => {
 				expect(data).to.be.a('string');
 			})
@@ -27,7 +30,7 @@ describe('Module', () => {
 		});
 
 		it('should return a string starting with our scheme identifyer', done => {
-			header('my key id', 'my secret key', 'the payload')
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today)
 			.then(data => {
 				expect(data.substr(0, 4)).to.equal('ss1 ');
 			})
@@ -35,18 +38,9 @@ describe('Module', () => {
 		});
 
 		it('should return a string with proper length', done => {
-			header('my key id', 'my secret key', 'the payload')
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today)
 			.then(data => {
-				expect(data.length).to.equal(310);
-			})
-			.done(done);
-		});
-
-		it('should use the time passed to it', done => {
-			let testtime = 1234567890000;
-			header('my key id', 'my secret key', 'the payload', testtime)
-			.then(data => {
-				expect(data).to.contain(', time=' + testtime);
+				expect(data.length).to.equal(290);
 			})
 			.done(done);
 		});
@@ -56,13 +50,13 @@ describe('Module', () => {
 	describe('verifying header', () => {
 
 		it('should provide a callback interface', done => {
-			header.verify('the header str', 'the payload', (keyid, cb) => { cb(null, 'the secret key'); }, (err, data) => {
+			header.verify('the header str', 'POST', '/endpoint', 'the payload', today, (keyid, cb) => { cb(null, 'the secret key'); }, (err, data) => {
 				done();
 			});
 		});
 
 		it('should provide a promise interface', done => {
-			header.verify('the header str', 'the payload', (keyid, cb) => { cb(null, 'the secret key'); })
+			header.verify('the header str', 'POST', '/endpoint', 'the payload', today, (keyid, cb) => { cb(null, 'the secret key'); })
 			.then(data => {
 				done();
 			})
@@ -72,17 +66,17 @@ describe('Module', () => {
 		});
 
 		it('should be able to verify headers created by the module', done => {
-			header('my key id', 'my secret key', 'the payload')
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today)
 			.then(data => {
-				return header.verify(data, 'the payload', (keyid, cb) => { cb(null, 'my secret key'); });
+				return header.verify(data, 'POST', '/endpoint', 'the payload', today, (keyid, cb) => { cb(null, 'my secret key'); });
 			})
 			.done(done);
 		});
 
 		it('should fail when secret key is not the same', done => {
-			header('my key id', 'my secret key', 'the payload')
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today)
 			.then(data => {
-				return header.verify(data, 'the payload', (keyid, cb) => { cb(null, 'another secret key'); });
+				return header.verify(data, 'POST', '/endpoint', 'the payload', today, (keyid, cb) => { cb(null, 'another secret key'); });
 			})
 			.then(() => {
 				done(new Error('Should not get here.'));
@@ -98,9 +92,9 @@ describe('Module', () => {
 		});
 
 		it('should fail when payload is not the same', done => {
-			header('my key id', 'my secret key', 'the payload')
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today)
 			.then(data => {
-				return header.verify(data, 'different payload', (keyid, cb) => { cb(null, 'my secret key'); });
+				return header.verify(data, 'POST', '/endpoint', 'different payload', today, (keyid, cb) => { cb(null, 'my secret key'); });
 			})
 			.then(() => {
 				done(new Error('Should not get here.'));
@@ -116,7 +110,7 @@ describe('Module', () => {
 		});
 
 		it('should fail if time diff is more than 24h', done => {
-			header.verify('ss1 keyid=my key id, hash=afc7e506a3e77a55d64ad2744d0f7a02d3bd9128ef1bf5eff04620c4b6fdd4e417b70893566edcef6ada6a4c5d76099c98bb06bfec5b93a1be793fdaba808ab9, nonce=6591a74c5b9e6c2b41d371b93bf08c12618ed866c000746dfca5764f6706666ea2ed1fbf4538313f2fb7da30a509a3423e976008734c73918751c1b403a24373, time=1000000000000')
+			header.verify('ss1 keyid=my key id, hash=afc7e506a3e77a55d64ad2744d0f7a02d3bd9128ef1bf5eff04620c4b6fdd4e417b70893566edcef6ada6a4c5d76099c98bb06bfec5b93a1be793fdaba808ab9, nonce=6591a74c5b9e6c2b41d371b93bf08c12618ed866c000746dfca5764f6706666ea2ed1fbf4538313f2fb7da30a509a3423e976008734c73918751c1b403a24373',  'POST', '/endpoint', 'the payload', yesturday)
 			.then(() => {
 				done(new Error('Should not get here.'));
 			})
@@ -130,7 +124,7 @@ describe('Module', () => {
 		});
 
 		it('should accept params in non-standard order', done => {
-			header('my key id', 'my secret key', 'the payload')
+			header('my key id', 'my secret key', 'POST', '/endpoint', 'the payload', today)
 			.then(data => {
 
 				let part = data.split(',').map(str => str.trim())
@@ -140,16 +134,16 @@ describe('Module', () => {
 					return pre;
 				}, {});
 
-				return 'ss1 time=' + part['time'] + ', nonce=' + part['nonce'] + ', hash=' + part['hash'] + ', keyid=' + part['keyid'];
+				return 'ss1 nonce=' + part['nonce'] + ', hash=' + part['hash'] + ', keyid=' + part['keyid'];
 			})
 			.then(data => {
-				return header.verify(data, 'the payload', (keyid, cb) => { cb(null, 'my secret key'); });
+				return header.verify(data, 'POST', '/endpoint', 'the payload', today, (keyid, cb) => { cb(null, 'my secret key'); });
 			})
 			.done(done);
 		});
 
 		it('should fail if format has too many params', done => {
-			header.verify('ss1 keyid=my key id, realm=wtf, hash=afc7e506a3e77a55d64ad2744d0f7a02d3bd9128ef1bf5eff04620c4b6fdd4e417b70893566edcef6ada6a4c5d76099c98bb06bfec5b93a1be793fdaba808ab9, nonce=6591a74c5b9e6c2b41d371b93bf08c12618ed866c000746dfca5764f6706666ea2ed1fbf4538313f2fb7da30a509a3423e976008734c73918751c1b403a24373, time=' + (new Date()).getTime())
+			header.verify('ss1 keyid=my key id, realm=wtf, hash=afc7e506a3e77a55d64ad2744d0f7a02d3bd9128ef1bf5eff04620c4b6fdd4e417b70893566edcef6ada6a4c5d76099c98bb06bfec5b93a1be793fdaba808ab9, nonce=6591a74c5b9e6c2b41d371b93bf08c12618ed866c000746dfca5764f6706666ea2ed1fbf4538313f2fb7da30a509a3423e976008734c73918751c1b403a24373', 'POST', '/endpoint', 'the payload', today)
 			.then(() => {
 				done(new Error('Should not get here.'));
 			})
@@ -163,7 +157,7 @@ describe('Module', () => {
 		});
 
 		it('should fail if not all params are in the string', done => {
-			header.verify('ss1 keyid=my key id, hash=afc7e506a3e77a55d64ad2744d0f7a02d3bd9128ef1bf5eff04620c4b6fdd4e417b70893566edcef6ada6a4c5d76099c98bb06bfec5b93a1be793fdaba808ab9, time=' + (new Date()).getTime())
+			header.verify('ss1 keyid=my key id, hash=afc7e506a3e77a55d64ad2744d0f7a02d3bd9128ef1bf5eff04620c4b6fdd4e417b70893566edcef6ada6a4c5d76099c98bb06bfec5b93a1be793fdaba808ab9', 'POST', '/endpoint', 'the payload', today)
 			.then(() => {
 				done(new Error('Should not get here.'));
 			})
